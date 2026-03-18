945f7d92ccc836be74b45426b4bdd022165a1401
Tiago Andrade <tiagobbandrade2704@gmail.com>
2026-03-18 14:27:45 -0300

    feat: add mock data and interfaces for group dashboard functionality

---
+++ src/data/mocks/interface.ts
@@ -0,0 +1,50 @@
+import type {
+  GroupBalanceItem,
+  GroupFinancialSummary,
+  ExpenseCategorySummary,
+  RecentActivityItem,
+} from "../../types/interfaces/dashboard/dashboard.types";
+import type { ExpenseDetails } from "../../types/interfaces/expenses/expenses.types";
+import type {
+  Group,
+  GroupDetails,
+} from "../../types/interfaces/groups/groups.types";
+import type { SettlementDetails } from "../../types/interfaces/settlements/settlements.types";
+import type { User } from "../../types/interfaces/users/user.types";
+
+export interface ApiResponse<T> {
+  data: T;
+  message?: string;
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  meta: {
+    page: number;
+    perPage: number;
+    total: number;
+    totalPages: number;
+  };
+}
+
+export type GetCurrentUserResponse = ApiResponse<User>;
+
+export type GetGroupsResponse = ApiResponse<Group[]>;
+
+export type GetGroupByIdResponse = ApiResponse<GroupDetails>;
+
+export type GetGroupExpensesResponse = ApiResponse<ExpenseDetails[]>;
+
+export type GetGroupSettlementsResponse = ApiResponse<SettlementDetails[]>;
+
+export type GetGroupBalancesResponse = ApiResponse<GroupBalanceItem[]>;
+
+export type GetGroupDashboardResponse = ApiResponse<{
+  group: GroupDetails;
+  summary: GroupFinancialSummary;
+  balances: GroupBalanceItem[];
+  recentExpenses: ExpenseDetails[];
+  settlements: SettlementDetails[];
+  categorySummary: ExpenseCategorySummary[];
+  recentActivity: RecentActivityItem[];
+}>;
