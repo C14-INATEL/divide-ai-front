24c07d31772c41b803f03f0bedb4c6675057ff7f
LeonardoFerreira23 <leonardo.fer201@gmail.com>
2026-05-14 22:01:25 -0300

    feat: integrate home page with real API and update axios baseURL

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -7,6 +7,11 @@ export type Group = {
 };
 
 export async function getGroups(): Promise<Group[]> {
-  const { data } = await http.get<Group[]>("/groups");
+const { data } = await http.get("/groups/", {
+    headers: {
+      Authorization: 
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlZmE0ZTIzNS05NDM5LTQwODYtOTBiOC1kZGNmN2M4ZmFlZDciLCJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJleHAiOjE3Nzg4ODU5NTN9.L93zGpMR2Pf-WwHgHXIe5cHNFqysUAPVaCUBhGFQF8Q"
+    } //Alterar quando integrar o login 
+  });
   return data;
 }
